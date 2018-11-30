d0176644a2db665c60723ff6e0d2b817e8865ab4
abhibhrdbg <god2cgr8ho#>
2018-12-01 00:13:09 +0530

    added boiler plate code for express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express    = require('express');
+const path       = require('path');
+const bodyParser = require('body-parser');
+const mongoose   = require('mongoose');
+const cors       = require('cors');
+
+const port      =   3000;
+const app       =   express();
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.jason());
+app.use(express.static(path.join(__dirname , 'public')));
+app.listen(port, () =>{
+    console.log(`Starting server on port ${port}`);
+});
