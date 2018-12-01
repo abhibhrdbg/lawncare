8026368f3431dff166a4707930888931210cdca1
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2018-12-01 16:41:33 +0530

    Added angular service for lawn and application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LawnService } from './services/lawn.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LawnService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
