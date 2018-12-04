19e271b6b958f0800b879ad7cb35c8b9b7748dc5
Abhishek Bhardwaj <volage.abhishek@gmail.com>
2018-12-04 18:29:39 +0530

    added app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {RouterModule, Routes } from '@angular/router'; 
+
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LawnService } from './services/lawn.service';
 import { ViewLawnComponent } from './components/view-lawn/view-lawn.component';
-import { ApplicationComponent } from './components/application/application.component';
 import { ViewApplicationComponent } from './components/view-application/view-application.component';
 import { EditLawnComponent } from './components/edit-lawn/edit-lawn.component';
 import { EditApplicationComponent } from './components/edit-application/edit-application.component';
 import { AddLawnComponent } from './components/add-lawn/add-lawn.component';
 import { AppApplicationComponent } from './components/app-application/app-application.component';
 
+const appRoutes : Routes = [{
+  path: 'lawns',
+  component: ViewLawnComponent
+  },
+  {
+    path: 'lawns/:id',
+    component: ViewApplicationComponent
+  },
+  {
+    path: '',
+    redirectTo: 'lawns',
+    pathMatch: 'full'
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ViewLawnComponent,
-    ApplicationComponent,
     ViewApplicationComponent,
     EditLawnComponent,
     EditApplicationComponent,
@@ -25,7 +41,9 @@ import { AppApplicationComponent } from './components/app-application/app-applic
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [LawnService],
   bootstrap: [AppComponent]
